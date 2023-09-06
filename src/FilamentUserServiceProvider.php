1b19a07e6dc7767f6df592534504ff50905023bd
dearvn <dearvn@users.noreply.github.com>
2023-09-06 08:36:42 +0000

    Fix styling

--- src/FilamentUserServiceProvider.php
+++ src/FilamentUserServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Dearvn\FilamentUser;
 
+use Dearvn\FilamentUser\Commands\FilamentUserCommand;
+use Dearvn\FilamentUser\Testing\TestsFilamentUser;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Dearvn\FilamentUser\Commands\FilamentUserCommand;
-use Dearvn\FilamentUser\Testing\TestsFilamentUser;
 
 class FilamentUserServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Dearvn\FilamentUser\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use Dearvn\FilamentUser\FilamentUserServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -18,7 +19,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Dearvn\FilamentUser\FilamentUserServiceProvider;
 
 class TestCase extends Orchestra
 {
